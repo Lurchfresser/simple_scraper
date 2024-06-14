ef514f11ea9de4601ef4874addaf722a96a36f49
tmeyne <Tritrop007@gmx.de>
2024-06-15 00:15:26 +0200

    extract user and now can handle infinite scrolling correctly

--- python/main.py
+++ python/main.py
@@ -7,15 +7,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from seleniumbase.common.exceptions import NoSuchElementException as SBNoSuchElementException
 
+def getIndex(l,id):
+    try:
+        for (i,elem) in enumerate(l):
+            if elem.get_property("id") == id:
+                return i + 1
+    except:
+        return 0
+    return 0
+
 def get_shadow_root(element,sb):
     return sb.execute_script('return arguments[0].shadowRoot', element)
 
 
 def extractInfos(elem,sb):
-    href = elem.get_attribute("href")
-    print("link: ",href)
-    text = elem.text
     try:
+        href = elem.find_element(By.CSS_SELECTOR,'a:first-of-type').get_attribute("href")
+        print("link: ",href)
+        user_elem = elem.find_element(By.CSS_SELECTOR,'faceplate-hovercard > faceplate-tracker > a > span:nth-of-type(2)')
+        user_text = user_elem.text.split("u/")[1]
+        print("User: ",user_text)
         shadow_root = get_shadow_root(elem, sb)
         faceplate_elems = shadow_root.find_elements(By.CSS_SELECTOR, 'faceplate-number')
         #comments_elem = shadow_root.find_element(By.XPATH, 'faceplate-number')
@@ -27,11 +38,14 @@ def extractInfos(elem,sb):
         timeElem = elem.find_element(By.XPATH,"..//time")
         time = timeElem.get_attribute("datetime")
         date_obj = parser.parse(time)
+        topic = elem.find_element(By.CLASS_NAME,'flair-content').text
+        print("Topic: ",topic)
         print(date_obj)
     except NoSuchElementException as e:
         print("Faceplate number element not found. This might not be an error depending on the page structure.")
     except Exception as e:
         print("An unexpected error occurred:", e) 
+    print("--------------------------------------------------")
     
 
 
@@ -40,25 +54,28 @@ with SB(uc=True, binary_location="/home/lurchfresser/Downloads/ungoogled-chromiu
     url = 'https://www.reddit.com/r/CryptoCurrency/new/'
     sb.driver.uc_open_with_reconnect(url, 3)
     length = 0
+    last_id = ""
     lenForScroll = 0
     while True:
-        elems = sb.find_elements("shreddit-post")
+        elems = sb.find_elements(By.TAG_NAME,"shreddit-post")
         print("length: ",len(elems))
-        for elem in elems[length:]:
+        index = getIndex(elems,last_id)
+        for elem in elems[index:]:
             extractInfos(elem,sb=sb)
-        length = len(elems)
         lenForScroll = len(sb.find_elements("shreddit-feed > *"))
+        last_id = elems[-1].get_property("id")
+        #TODO: it still needs one iteration
         try:
             print("length for scroll: ",lenForScroll)
-            sb.scroll_to(f"shreddit-feed > *:nth-child({lenForScroll})")
+            #sb.scroll_to(f"shreddit-feed > *:nth-child({lenForScroll})")
+            sb.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         except SBNoSuchElementException as e:
-            print("No more elements to wait for.")
+            print("No more elements to scroll to")
             break
         try:
-            sb.wait_for_query_selector(f"shreddit-feed > *:nth-child({lenForScroll+1})")
+            sb.wait_for_query_selector(f"shreddit-feed > *:nth-child({lenForScroll+1})", timeout=20)
         except NoSuchElementException as e:
-            print("No more elements to scroll to.")
-            break
+            print("No more elements to wait for.")
     print("Done")
     #sb.driver.quit()
     sb.wait(1000)
